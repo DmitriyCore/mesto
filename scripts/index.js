1d615b4b78d96aabc5a324f90f5c52838f705ead
Dmitriy Razmanov <kr_dmitriy@outlook.com>
2023-07-12 02:18:59 +0700

    fixed defects from the second review

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,12 @@
 /* Попап Edit и форма содержащая поля ввода */
 
 const popupEdit = document.querySelector('.popup_type_edit');
-const formElement = document.querySelector('form');
-
-/* Проверка на наличие класса "popup_opened" */
-
-let isHidden = popupEdit.classList.contains('popup_opened');
+const form = document.querySelector('.popup__info');
 
 /* Переменные для полей ввода формы */
 
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_job');
+let nameInput = form.querySelector('.popup__input_type_name');
+let jobInput = form.querySelector('.popup__input_type_job');
 
 /* Переменные для полей профиля */
 
@@ -29,29 +25,26 @@ buttonClose.addEventListener('click', popupClose);
 
 /* Нажатие на кнопку Submit */
 
-const buttonSubmit = popupEdit.querySelector('.popup__submit');
-buttonSubmit.addEventListener('click', handleFormSubmit);
+document.addEventListener('submit', popupSubmit);
 
 /* Функция открытия формы */
 
 function popupOpen () {
-  if (isHidden == false) {
-    popupEdit.classList.toggle('popup_opened');
-    nameInput.value = nameField.textContent;
-    jobInput.value = jobField.textContent;
-  }
+  popupEdit.classList.add('popup_opened');
+  nameInput.value = nameField.textContent;
+  jobInput.value = jobField.textContent;
 }
 
 /* Функция закрытия формы */
 
 function popupClose () {
-  popupEdit.classList.toggle('popup_opened');
+  popupEdit.classList.remove('popup_opened');
 }
 
 /* Функция отправки формы */
 
-function handleFormSubmit (evt) {
-  evt.preventDefault();
+function popupSubmit (event) {
+  event.preventDefault()
   popupClose ();
   nameField.textContent = nameInput.value;
   jobField.textContent = jobInput.value;
